b6765ac52083fc80d7fd1a810b1e92d88ac05b16
Commando1901 <patelmandip1901@gmail.com>
2026-01-29 23:03:21 +0530

    Temporarily enable DEBUG for troubleshooting

--- Hello/settings.py
+++ Hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-gvoh9)qiz_ut_dg1i-393sb7u-v*ddpj4sq%yhl)1%w%by9a+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
